55ec9ccdb5c8ce1485e225fa799d5468d4b8714c
John W. Kilgo VI <jkilgo@mitre.org>
2016-06-06 15:24:10 -0400

    update js

--- marker-resize.js
+++ marker-resize.js
@@ -30,30 +30,34 @@ L.Marker.AutoResize = L.Marker.extend({
 		icon: null
 	},
 
-
+	// Override initialize
 	initialize: function (latlng, options) {
 		L.setOptions(this, options);
 
 		var options = this.options;
 
-		// Did the user supply 3 icons? If not, let's help them out
+		// Did the user supply 3 icons?
 		if (options.iconArray.length !== 3) {
+			console.log("WARNING :: 3 icons not defined in L.Marker.AutoResize, an inefficient Marker will be produced.")
 			if (options.iconArray[0] instanceof L.Icon) {
 				options.iconArray[1] = options.iconArray[0];
-				options.iconArray[2] = options.iconArray[1];
+				options.iconArray[2] = options.iconArray[0];
 			} else {
 				options.iconArray = [new L.Icon.Default(),
-					 	     new L.Icon.Default(),
-						     new L.Icon.Default()];
+					 	     		 new L.Icon.Default(),
+						     		 new L.Icon.Default()];
 			}
 		}
 
 		// Set the default icon, mid-range zoom level
-		options.icon = options.iconArray[2];
+		// Used by _initIcon override
+		// A value between 0 and 2 (since three icons are allowed to be zoomed)
+		this._initIconParam = 1;
 
 		this._latlng = L.latLng(latlng);
 	},
 
+	// Override update 
 	update: function () {
 
 		if (this._icon) {
@@ -67,11 +71,10 @@ L.Marker.AutoResize = L.Marker.extend({
 		return this;
 	},
 
-
+	// Gets the current zoom from the map and applies appropriate icon
 	_updateZoomend: function() {
 
 		var map = this._map,
-		    icons = this.options.iconArray,
 		    zoom = map.getZoom(),
 		    min = isFinite(map.getMinZoom()) ? map.getMinZoom() : 2,
 		    max = isFinite(map.getMaxZoom()) ? map.getMaxZoom() :20,
@@ -80,46 +83,158 @@ L.Marker.AutoResize = L.Marker.extend({
 		zoomed = Math.ceil((max - min) * (2/3) + min);
 		outer  = Math.ceil((max - min) * (1/3) + min);
 
-		/**/
+		/*
 		console.log("in _updateZoomend")
 		console.log("	zoom: " + zoom)
 		console.log("	min: " + min)
 		console.log("	max: " + max)
 		console.log("	zoomed: " + zoomed)
 		console.log("	outer: " + outer)
-		/**/
+		*/
 
 		if (zoom >= zoomed) {
-			if (this.options.icon === icons[2]) {
-				return;
-			} else {
-				this.options.icon = icons[2];
-				this._initIcon(); 
-			}
-
-		} else if (zoom < zoomed && zoom > outer) {
-			if (this.options.icon === icons[1]) {
-				return;
-			} else {
-				this.options.icon = icons[1];
-				this._initIcon();
-			}
-	
-		} else if (zoom <= outer) {
-			if (this.options.icon === icons[0]) {
-				return;
-			} else {
-				this.options.icon = icons[0];
-				this._initIcon();
-			}
-	
+			this._initIconParam = 2;
+			this._initIcon(); 
+		}
+		else if (zoom < zoomed && zoom > outer) {
+			this._initIconParam = 1;
+			this._initIcon();
+		}
+		else if (zoom <= outer) {
+			this._initIconParam = 0;
+			this._initIcon();
 		}
 	},
 
+	// Override _initIcon()
+	_initIcon: function () {
+
+		// objects used by internal functions: this._shadow, this._icon
+		// _initIconCurrent = current icon actually on the map
+		// _initIconParam = icon that is requested
+		// _iconArrayInitialized = ensures that the intialization is only performed once
+
+		// TODO:
+		// 1. Override setIcon(), set this._icon to null to trigger new creation of icon?????
+
+		var options = this.options
+		var classToAdd = 'leaflet-zoom-' + (this._zoomAnimated ? 'animated' : 'hide');
+
+		if(!this._iconArrayInitialized) {
+
+			// init to be able to use
+			this._iconArray = [];
+			this._shadowArray = [];
+
+			// Create the icons and shadows
+			for (var i = 0; i < 3; i++) {
+				this._iconArray[i] = options.iconArray[i].createIcon();
+				this._shadowArray[i] = options.iconArray[i].createShadow();
+
+				// Give icons title/alt text if options call for them
+				if (options.title) {
+					this._iconArray[i].title = options.title;
+				}
+				if (options.alt) {
+					this._iconArray[i].alt = options.alt;
+				}
+				if (options.keyboard) {
+					this._iconArray[i].tabIndex = '0';
+				}
+
+			}
+
+			if (options.riseOnHover) {
+				this.on({
+					mouseover: this._bringToFront,
+					mouseout: this._resetZIndex
+				});
+			}
+
+			// Keep track of current icon
+			// Set to a number different from this._initIconParam to trigger creation of icon
+			this._initIconCurrent = 0;
+			this._iconArrayInitialized = true;
+
+		}
+
+		// Where the icons are actually changed
+		// We want it to continue to the second part after initialilzation
+
+		// Different icon requested from existing icon
+		if (this._initIconCurrent !== this._initIconParam) {
+			// No icon exists
+			if (!this._icon) {
+
+				this._icon = this._iconArray[this._initIconParam];
+
+				L.DomUtil.addClass(this._icon, classToAdd);
+
+				if (options.riseOnHover) {
+					this.on({
+						mouseover: this._bringToFront,
+						mouseout: this._resetZIndex
+					});
+				}
+
+				// Shadows
+				if (this._shadowArray[this._initIconParam]) {
+					this._shadow = this._shadowArray[this._initIconParam];
+					L.DomUtil.addClass(this._shadow, classToAdd);
+					this.getPane('shadowPane').appendChild(this._shadow);
+				}
+
+				if (options.opacity < 1) {
+					this._updateOpacity();
+				}
+
+				this.getPane().appendChild(this._icon);
+				this._initInteraction();
+
+
+			} else { // Icon exists
+
+				// replace icons 
+				this._removeIcon();
+				// remove existing shadow if it exists
+				if (this._shadowArray[this._initIconCurrent])
+					this._removeShadow();
+
+				this._initIconCurrent = this._initIconParam;
+
+				this._icon = this._iconArray[this._initIconParam];
+				L.DomUtil.addClass(this._icon, classToAdd);
+
+				if (options.riseOnHover) {
+					this.on({
+						mouseover: this._bringToFront,
+						mouseout: this._resetZIndex
+					});
+				}
+
+				// Shadows
+				if (this._shadowArray[this._initIconParam]) {
+					this._shadow = this._shadowArray[this._initIconParam];
+					L.DomUtil.addClass(this._shadow, classToAdd);
+					this.getPane('shadowPane').appendChild(this._shadow);
+				}
+
+				if (options.opacity < 1) {
+					this._updateOpacity();
+				}
+
+				this.getPane().appendChild(this._icon);
+				this._initInteraction();
+
+			}
+
+		} else { // Same icon requested, let's exit
+			return;
+		}
+	}
 
 });
 
 L.autoResizeMarker = function(latlng, options) {
 	return new L.Marker.AutoResize(latlng, options);
 };
-
